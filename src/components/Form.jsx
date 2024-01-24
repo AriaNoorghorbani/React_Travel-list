eb470b7593d076ae5898f65ec5a5e9d7073a8d35
arianoorqorbani <aria.noorghorbani@gmail.com>
2024-01-24 15:32:46 +0300

    Save form data

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Form() {
+  const [description, setDescription] = useState();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const newItem = description;
+    console.log(newItem);
+    setDescription("");
+  }
+
   return (
     <div className="add-form">
       <h3>What do you need for your trip?</h3>
-      <form className="add-form">
-        <input type="text" />
+      <form className="add-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={description}
+          onChange={(event) => setDescription(event.target.value)}
+        />
         <select name="" id="">
           {Array.from({ length: 20 }, (_, i) => i + 1).map((i) => (
             <option key={i}>{i}</option>
