63fee2f6c45205643d7646d2ad1d539e415b0dfc
arianoorqorbani <aria.noorghorbani@gmail.com>
2024-01-24 14:33:38 +0300

    Implement form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,6 +4,15 @@ export default function Form() {
   return (
     <div className="add-form">
       <h3>What do you need for your trip?</h3>
+      <form className="add-form">
+        <input type="text" />
+        <select name="" id="">
+          {Array.from({ length: 20 }, (_, i) => i + 1).map((i) => (
+            <option>{i}</option>
+          ))}
+        </select>
+        <button>Add</button>
+      </form>
     </div>
   );
 }
